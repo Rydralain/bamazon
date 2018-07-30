3b77f54f21a0f5d054451ec3f0239a43c90f27af
Rydralain <37129740+Rydralain@users.noreply.github.com>
2018-07-30 09:53:47 -0700

    added sample mysql data & added prompts for selecting items

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,11 +16,31 @@ const inquirer = require('inquirer');
 
 function itemSelect(){
     // fetch all in-stock items
-    // display them as a selectable list in inquirer (and cancel option)
-    // after, allow quantity select
+    connection.query("select * from products", function(err, res){
+        if(err) throw err;
+        var itemsList = []
+        // making array for inquirer
+        for(let i = 0; i < res.length; i++){
+            itemsList[i] = [];
+            itemsList[i].name = res[i].product_name + " - $" + res[i].price + " - [" + res[i].stock_quantity + "]";
+            itemsList[i].value = res[i].item_id;
+        }
+        itemsList[res.length] = "CANCEL";
+        // display them as a selectable list in inquirer
+        listQuestion = {
+            type : "list",
+            name : "productSelect",
+            message : "Select a product",
+            choices : itemsList
+        };
+        inquirer.prompt(listQuestion).then(answers => {
+            // after, allow quantity select
+            console.log("selected ID: " + answers.productSelect);
+        });
+    });
     // check if selected quantity is valid
     // send db update
-    purchaseItem(item, quantity);
+    //purchaseItem(item, quantity);
 }
 
 function purchaseItem(item, quantity){
--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -5,6 +5,9 @@ FUNCTIONALITY SUMMARY
     (restart/quit)
 */
 
+// this code would all be a little better if I was builidng a department object to store data,
+// but I'm past out of time for this assignment, so an oop redesign is out of scope - I may not even get to this file at all
+
 // premade mysql connection to the db
 const connection = require('./bamazonConnect.js');
 
@@ -24,6 +27,9 @@ function showSalesPrompts(){
 }
 
 function showSales(deptID){
-    // 
+    // query for dept name & overhead
+    // query for transactions for this dept
+    // add up transaction revenue
+    // display table
 }
 
