acad0ea697d5605258919f013906aa9cd2bfbb50
Rydralain <37129740+Rydralain@users.noreply.github.com>
2018-08-01 09:51:22 -0700

    product inventory update and completed all input loops

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -45,13 +45,14 @@ function itemSelect(){
             // after, allow quantity select
             let selected = {}
             selected = res[answers.productSelect];
-            selectQuantity(selected.product_id, selected.stock_quantity);
+            selectQuantity(selected.item_id, selected.stock_quantity);
         });
     });
 }
 
 function selectQuantity(itemID, stockLevel){
     // prep question
+    console.log(itemID, 1)
     let getQuantity = {
         type: "input",
         name: "quantity",
@@ -60,6 +61,7 @@ function selectQuantity(itemID, stockLevel){
     inquirer.prompt(getQuantity).then(answers => {
         let quantity = parseInt(answers.quantity);
         if(quantity <= stockLevel){
+            console.log(itemID, 2)
             purchaseItem(itemID, quantity);
         }
         else if(answers.quantity == "exit"){
@@ -73,14 +75,30 @@ function selectQuantity(itemID, stockLevel){
     });
 }
 
-function purchaseItem(item, quantity){
+function purchaseItem(itemID, quantity){
     // send query
-    connection.query("UPDATE products SET stock_quantity -= ? where ?", [quantity, {item_id: quantity}], function(err, res){
+    console.log(itemID, 3)
+    connection.query("UPDATE products SET stock_quantity = stock_quantity - ? where ?", [quantity, {item_id: itemID}], function(err, res){
         if(err) throw err;
         // return results
         console.log("Purchase complete");
-        itemSelect();
         // ask to continue or end
+        let restart = {
+            type: "list",
+            name: "restart",
+            message: "Restart or exit?",
+            choices: ["Restart", "Exit"]
+        }
+        inquirer.prompt(restart).then(answers => {
+            if(answers.restart === "Restart"){
+                itemSelect();
+            }
+            else{
+                console.log("Exiting.");
+                connection.end();
+                return
+            }
+        })
     });
 }
 
